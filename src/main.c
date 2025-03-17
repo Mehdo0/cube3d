defd285f757f019899714be9c8009fb52db5b138
Mohamed Mouaffak <mmouaffa@c2r9s9.42lausanne.ch>
2025-03-17 14:31:34 +0100

    l

--- src/main.c
+++ src/main.c
@@ -5,12 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmouaffa <mmouaffa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/28 15:52:15 by mmouaffa          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/03/17 14:05:29 by mmouaffa         ###   ########.fr       */
-=======
-/*   Updated: 2025/03/17 14:04:35 by kgiraud          ###   ########.fr       */
->>>>>>> 5d2dcfa9cc2a400e4f35b232079ebcd1e1ac518e
+/*   Created: 2025/03/17 14:19:01 by mmouaffa          #+#    #+#             */
+/*   Updated: 2025/03/17 14:19:03 by mmouaffa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +14,6 @@
 
 int	main(int ac, char **av)
 {
-<<<<<<< HEAD
 	if (ac != 2)
 		ft_map_error("Error\nInvalid number of arguments\n");
 	t_config *config = ft_init_and_parse_map(av[1]);	
@@ -35,13 +30,12 @@ int	main(int ac, char **av)
 	void *mlx = mlx_init();
 	mlx_new_window(mlx, 1900, 1000, "oui");
 	mlx_loop(mlx);
-=======
+
 	t_env	env;
 	
 	env.mlx = mlx_init();
 	env.win = mlx_new_window(env.mlx, 1920, 1080, "Les blagues room");
 	handle_hook(&env);
 	mlx_loop(env.mlx);
->>>>>>> 5d2dcfa9cc2a400e4f35b232079ebcd1e1ac518e
 	return (0);
 }
