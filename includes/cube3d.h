57c68539070c0934c8d37c7ab375f24683f55ee1
Mohamed Mouaffak <mmouaffa@c2r9s9.42lausanne.ch>
2025-03-17 13:15:16 +0100

    l

--- includes/cube3d.h
+++ includes/cube3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cube3d.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kgiraud <kgiraud@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*   By: mmouaffa <mmouaffa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 12:41:34 by kgiraud           #+#    #+#             */
-/*   Updated: 2025/03/03 13:05:11 by kgiraud          ###   ########.fr       */
+/*   Updated: 2025/03/07 11:24:47 by mmouaffa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,13 @@
 
 # include <stdlib.h>
 # include <stdio.h>
+
+typedef struct s_player
+{
+    float	x;
+    float	y;
+    float	dir;
+    float	fov;
+}    t_player;
 
 #endif
